eacc5dd7e5f4aba96e775338e5d0ab6a175642b4
Alexey Marichev <aleshamarichev09@gmail.com>
2024-11-05 23:43:47 +0300

    +base+ref+love_connect

--- defs/defd.py
+++ defs/defd.py
@@ -3,7 +3,6 @@ import sqlite3
 def ref_prov(id_use):
     sqlite_connection = sqlite3.connect('bd/bd')
     cursor = sqlite_connection.cursor()
-    print("Подключен к SQLite")
     sqlite_select_query = """SELECT * from users"""
     cursor.execute(sqlite_select_query)
     records = cursor.fetchall()
@@ -13,8 +12,36 @@ def ref_prov(id_use):
             return True
     cursor.close()
     return False
+#проверка есть ли юзер в бд
+
+
+
 def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
     conn = sqlite3.connect('bd/bd', check_same_thread=False)
     cursor = conn.cursor() #курсор для бд
     cursor.execute('INSERT INTO users (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username))
     conn.commit()
+#запись нового юзера в бд
+
+
+def from_bd(num: int, user_id: int):
+    sqlite_connection = sqlite3.connect('bd/bd')
+    cursor = sqlite_connection.cursor()
+    sqlite_select_query = """SELECT * from users"""
+    cursor.execute(sqlite_select_query)
+    records = cursor.fetchall()
+    for row in records:
+        if row[0]==user_id:
+            return row[num]
+    cursor.close()
+#забирает номер столбца бд кот нуден и пользователя возвращает значение заданнного столбца
+
+
+
+def zamena_para(user_id: int, smena):
+    sqlite_connection = sqlite3.connect('bd/bd')
+    cursor = sqlite_connection.cursor()
+    cursor.execute('UPDATE users SET para_id = ? WHERE user_id = ?', (smena, user_id))
+    sqlite_connection.commit()
+    sqlite_connection.close()
+#подмена элемента
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, F
-from aiogram.filters import Command, CommandStart, StateFilter
+from aiogram.filters import Command, CommandStart, StateFilter, CommandObject
 from aiogram.filters.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
@@ -10,7 +10,12 @@ import os
 import sqlite3
 import telebot
 from aiogram.types import FSInputFile
-from defs.defd import ref_prov, db_table_val
+from defs.defd import ref_prov, db_table_val, from_bd, zamena_para
+from aiogram.utils.deep_linking import create_start_link, decode_payload
+from aiogram import types
+import base64
+
+
 
 load_dotenv()
 
@@ -20,37 +25,95 @@ dp = Dispatcher()
 
 
 #инлайн кнопки
+url_button_my_otn=InlineKeyboardButton(
+     text="Мои отношения",
+     callback_data='button_my_otn'
+)
 url_button_1=InlineKeyboardButton(
-     text="Оформить покупку",
-     callback_data='button_buy_press'
+     text="Создать отношения",
+     callback_data='button_new_otn')
+url_button_return=InlineKeyboardButton(
+     text="Вернуться",
+     callback_data='button_return'
+)
+url_button_del_otn=InlineKeyboardButton(
+     text="Удалить отношения",
+     callback_data='button_del_otn'
+)
+url_button_del=InlineKeyboardButton(
+     text="ДА", #согласие на удаление отношений
+     callback_data='button_del'
 )
 #обьект инлайн клавиатуры
 keyboard_menu=InlineKeyboardMarkup(
      inline_keyboard=[[url_button_1]]
 )
+keyboard_menu_true=InlineKeyboardMarkup(
+     inline_keyboard=[[url_button_my_otn]]
+)
+keyboard_menu_return=InlineKeyboardMarkup(
+     inline_keyboard=[[url_button_return]]
+)
+
+
 
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
-    await message.answer(f'Вас приветствует бот по продаже вирт!\n'
+    await message.answer(f'Вас приветствует бот бот для парочек!\n'
+                         f'Здесь вы можете настраивать и развивать свои отношения\n'
                          f'Для взаимодействия отправьте /start\n'
                          )
 
-
 @dp.message(CommandStart)
-async def start_cm(message: Message):
+async def start_cm(message: types.Message):
     if ref_prov(message.from_user.id)==False:
         us_id = message.from_user.id
         us_name = message.from_user.first_name
         us_sname = message.from_user.last_name
         username = message.from_user.username
         db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
-    await message.answer_photo(photo=FSInputFile('image/menu.jpg', filename='кру'),
+    if from_bd(4, message.from_user.id)==None:
+        await message.answer_photo(photo=FSInputFile('image/menu.jpg', filename='кру'),
                         caption=f'Приветствую тебя в боте любви!\n'
-                        f'Для построения отношений введите /newpara',
+                        f'Для построения отношений выберите "Создать отношения"',
                         reply_markup=keyboard_menu
                         )
-    print("dddd")
+    else:
+        await message.answer_photo(photo=FSInputFile('image/menu.jpg', filename='кру'),
+                            caption=f'Приветствую тебя в боте любви!\n'
+                            f'Уже бежишь к своей половинке?',
+                            reply_markup=keyboard_menu_true)
+    if (message.text[7:])!="" and (message.text[7:])!=message.from_user.id:
+        zamena_para(int(decode_payload(message.text[7:])), message.from_user.id)
+        await message.answer(f'Теперь ВЫ и {from_bd(1, int(decode_payload(message.text[7:])))} пара!')
 
+
+
+
+#кнопка соззать отношения
+@dp.callback_query(F.data=='button_new_otn')
+async def mt_referal_menu (callback: CallbackQuery, state: FSMContext, bot: Bot):
+    link = await create_start_link(bot,str(callback.from_user.id), encode=True)
+    print("f")
+    print(link)
+    await callback.message.edit_caption(caption=f'Для того, что бы добавить пару\n'
+                         f'Ваша половинка должна перейти по следующей ссылке:\n'
+                         f'{link}',
+                         media=FSInputFile('image/love.jpg'),
+                         reply_markup=keyboard_menu_return
+                        )
+    await callback.answer()
+
+#смена кнопки по проходу меню-отношения-----------------edit
+@dp.callback_query(F.data=='button_my_otn')
+async def process_button_buy_press(callback: CallbackQuery):
+    if callback.message.text != "Ваши отношения":
+        if from_bd(4, callback.from_user.id):
+            await callback.message.edit_caption(
+                caption="Ваши отношения",
+                reply_markup=keyboard_menu_buy
+        )
+    await callback.answer()
 
 
 # Запускаем поллинг
